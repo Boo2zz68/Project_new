2c0f2442c2789d2ed8f84c0ed4e307721d80c7a2
AButusov <boo2zz75384@mail.ru>
2022-10-17 21:27:29 +0300

    ДЗ_4

--- src/main/java/ДЗ_4/TriangleClass.java
+++ src/main/java/ДЗ_4/TriangleClass.java
@@ -1,6 +1,6 @@
 package ДЗ_4;
 
-public class Triangle {
+public class TriangleClass {
     public static int  triangle(int a, int b, int c) throws MyException {
         if (a+b<c || a+c<b || b+c<a) throw new MyException("Ошибка");
         int p = (a+b+c)/2;
